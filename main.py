d4b03b7646fc7f848ad821e6b1a4387db6966183
ATHARVA320-EDITOR <ddnatharva@gmail.com>
2024-11-18 21:23:28 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Half Pyramid Pattern of Stars(*):")
+n = int(input("Enter the number of rows you want: "))
+for i in range(n):
+    for j in range(i + 1):
+        print("*", end=" ")
+    print()
